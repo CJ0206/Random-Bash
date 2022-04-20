6ee624f4ab13ff240b41820b671f7e09f3918a83
CJ <CJ0206@users.noreply.github.com>
2022-04-20 21:48:54 +0100

    Update Backup TP to OneDrive.bat

--- Backup TP to OneDrive.bat
+++ Backup TP to OneDrive.bat
@@ -1,17 +1,18 @@
 :: Kill the Touch Portal process
 taskkill /im TouchPortal.exe
 
-:: Directory to save backups
+:: Directory to backup and directory where the backup should be saved
+SET oldDir=%AppData%\TouchPortal
 SET backupDir=%USERPROFILE%\OneDrive\Documents\TouchPortal
 
-:: Store date as a variable
+:: Store date and tome as a variable [DD-MM-YYYY HH-MM-SS]
 FOR /f "tokens=2 delims==" %%G in ('wmic os get localdatetime /value') do set datetime=%%G
 SET hh=%time:~0,2%
 SET hh=%hh: =0%
-SET datetime=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2% %hh%-%time:~3,2%-%time:~6,2%
+SET datetime=%datetime:~6,2%-%datetime:~4,2%-%datetime:~0,4% %hh%-%time:~3,2%-%time:~6,2%
 
-:: Zip folders
-C:\"Program Files"\7-Zip\7z A "%backupDir%\%datetime% TouchPortal.zip" "%AppData%\TouchPortal\*"
+:: Zip folder using 7zip and save to the backup directory with the name DD-MM-YYYY HH-MM-SS TouchPortal.zip
+C:\"Program Files"\7-Zip\7z A "%backupDir%\%datetime% TouchPortal.zip" "%oldDir%\*"
 
 :: Open backup directory
 start "" "%backupDir%"
